99f3dc4c32b1b2af784f2c8c29b35e9e14bef4d9
Justin Lemon <justinklemon@gmail.com>
2024-09-06 16:27:14 -0600

    Updated the changelog, version number and example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 ## 0.0.1
 Initial release technically works, but runs out of memory on larger lists. Will need to refactor to prevent this.
+## 0.0.2
+- Refactored to use a SetQueue instead of a Queue, helping with the memory problem. This also sped up crossword generation by making sure that we didn't follow the same path multiple times.
+- Refactored the hashCodeFromValuesOnly function to be a relativeHashCode function instead. It now takes into account the relative position of the values, not just the raw values. This make it work as intended with isRelativelyEquivalent/sets.
+- Fixed a bug with the placeWord function where words could be placed over each other.
--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,4 +1,5 @@
 import 'package:crossword_generator/crossword_generator.dart';
+import 'package:intl/intl.dart';
 
 void main(List<String> arguments) {
   List<String> listOfWords = [
@@ -7,19 +8,36 @@ void main(List<String> arguments) {
     'shop',
     'paws',
     'simplify',
-    'ballet', //268 crosswords
-    // Gets slow after this
-    // 'puffed', //3728 crosswords
-    //Stops working after this (runs out of memory)
-    // 'pealed',
-    // 'kills',
-    // 'fig'
+    'ballet',
+    'puffed',
+    // Noticeably slower after these words are added
+    'pealed',
+    'kills',
+    'fig'
   ];
-  List<Crossword> crosswords = generateCrosswords(listOfWords);
 
-  for (Crossword crossword in crosswords) {
-    print(crossword.toPrettyString());
-    print("Score: ${crossword.score}");
+  for (int i = 1; i < listOfWords.length + 1; i++) {
+    List<String> words = listOfWords.sublist(0, i);
+    print("Generating crosswords for $words");
+    final stopwatch = Stopwatch()..start();
+    List<Crossword> crosswords = generateCrosswords(words);
+    stopwatch.stop();
+    NumberFormat numberFormat = NumberFormat('#,##0');
+    String formattedCrosswordsLength = numberFormat.format(crosswords.length);
+    Duration elapsed = stopwatch.elapsed;
+    String formattedElapsedTime =
+        "${(elapsed.inMinutes % 60).toString().padLeft(2, '0')}:"
+        "${(elapsed.inSeconds % 60).toString().padLeft(2, '0')}:"
+        "${(elapsed.inMilliseconds % 1000).toString().padLeft(3, '0')}";
+
+    print("Found $formattedCrosswordsLength crosswords in $formattedElapsedTime");
+    // for (Crossword crossword in generateCrosswords(listOfWords)) {
+    //   print(crossword.toPrettyString());
+    //   print("---Placed words---}");
+    //   for (PositionedWord word in crossword.placedWords) {
+    //     print(word);
+    //   }
+    //   print("Score: ${crossword.score}");
+    // }    
   }
-  print("Found ${crosswords.length} crosswords");
 }
